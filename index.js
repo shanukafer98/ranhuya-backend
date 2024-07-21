0de39966bc91925882fbec437dc9380d802acec1
shanukafer98 <shanukafer98@gmail.com>
2024-07-21 08:33:31 +0530

    chore: Update server port to 3000

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const port = process.env.PORT || 3000;
+const port =  3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
