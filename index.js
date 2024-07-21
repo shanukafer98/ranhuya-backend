7297d9ee1f31891f8078d3a4e6fd0a4dbcda8d6d
shanukafer98 <shanukafer98@gmail.com>
2024-07-21 08:31:01 +0530

    chore: Update MongoDB connection options and error handling

--- index.js
+++ index.js
@@ -10,62 +10,42 @@ import cors from 'cors';
 
 dotenv.config();
 
+// Check if MONGO environment variable is set
+if (!process.env.MONGO) {
+  throw new Error('MONGO environment variable is not defined');
+}
+
 mongoose
-  .connect(process.env.MONGO)
+  .connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB!');
   })
   .catch((err) => {
-    console.log(err);
+    console.error('Failed to connect to MongoDB:', err);
+    process.exit(1); // Exit process with failure
   });
 
 const __dirname = path.resolve();
 
 const app = express(); // Move this line before using app
 
-
-app.use(cors());
-
-app.use(express.json());
-app.use(cookieParser());
-
-// Add COOP header middleware
-// app.use((req, res, next) => {
-//   res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
-//   next();
-// });
-
-
-
-// app.use(cors(
-//   {
-//     origin: ['http://localhost:5173'],
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     credentials: true,
-//   }
-// ));
-
-
-
+// Middleware
 app.use(cors({
-  origin: "https://ranhuya.vercel.app",
+  origin: "https://ranhuya.vercel.app", // Update with your production domain
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
 }));
 
+app.use(express.json());
+app.use(cookieParser());
 
+// Static files
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
+// Routes
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
-
-app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
@@ -73,15 +53,22 @@ app.get('*', (req, res) => {
 
 app.get('/', (req, res) => {
   res.send('Hello World, from express');
-})
+});
 
-
+// Error handling middleware
 app.use((err, req, res, next) => {
+  console.error('Error occurred:', err);
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
-  return res.status(statusCode).json({
+  res.status(statusCode).json({
     success: false,
     statusCode,
     message,
   });
+});
+
+// Start server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
