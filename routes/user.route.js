4272cc38a52e60be49fc2acc87588e42b3e52c8a
shanukafer98 <shanukafer98@gmail.com>
2024-07-26 11:11:52 +0530

    Make change in middleware

--- routes/user.route.js
+++ routes/user.route.js
@@ -6,9 +6,9 @@ import { verifyToken } from '../utils/verifyUser.js';
 const router = express.Router();
 
 router.get('/test', test);
-router.post('/update/:id', verifyToken, updateUser)
-router.delete('/delete/:id', verifyToken, deleteUser)
-router.get('/listings/:id', verifyToken, getUserListings)
-router.get('/:id', verifyToken, getUser)
+router.post('/update/:id', updateUser)
+router.delete('/delete/:id', deleteUser)
+router.get('/listings/:id', getUserListings)
+router.get('/:id', getUser)
 
 export default router;
