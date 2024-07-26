a18b4536e6704e7d8bd138a25a342f90c8fbced6
shanukafer98 <shanukafer98@gmail.com>
2024-07-26 16:44:21 +0530

    chore: Update cookie configuration for access_token

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -25,7 +25,7 @@ export const signin = async (req, res, next) => {
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser._doc;
     res
-      .cookie('access_token', token ,{httpOnly: true,secure:true})
+      .cookie('access_token', token ,{httpOnly: true})
       .status(200)
       .json(rest);
   } catch (error) {
